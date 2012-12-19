aee77e5938f40390585f173d52f97e66f9f73ab4
Brad Ison <bison@garbagebrain.org>
2012-12-19 13:44:47 -0600

    Only CA servers should generate their own cert

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,19 +11,22 @@ class puppetmaster::config {
     content => template($puppetmaster::puppet_conf_template),
   }
 
-  # Autosign needs to be explicitly set to false to avoid a bug
-  # in the cert generate command. Certs generated in this way
-  # are sign automatically regardless of whether autosign is
-  # enabled or not. See: http://projects.puppetlabs.com/issues/6112
-  $cert_file = "${::puppetmaster::ssldir}/certs/${::puppetmaster::certname}.pem"
-  $cert_command = "puppet cert generate --autosign false ${::puppetmaster::certname}"
+  # Only CA servers should generate their own cert.
+  if $::puppetmaster::ca {
+    # Autosign needs to be explicitly set to false to avoid a bug
+    # in the cert generate command. Certs generated in this way
+    # are sign automatically regardless of whether autosign is
+    # enabled or not. See: http://projects.puppetlabs.com/issues/6112
+    $cert_file = "${::puppetmaster::ssldir}/certs/${::puppetmaster::certname}.pem"
+    $cert_command = "puppet cert generate --autosign false ${::puppetmaster::certname}"
 
-  exec { 'puppetmaster::cert::generate':
-    path    => '/usr/bin:/bin',
-    command => $cert_command,
-    creates => $cert_file,
-    user    => 'root',
-    group   => 'root',
-    require => File["${puppetmaster::confdir}/puppet.conf"],
+    exec { 'puppetmaster::cert::generate':
+      path    => '/usr/bin:/bin',
+      command => $cert_command,
+      creates => $cert_file,
+      user    => 'root',
+      group   => 'root',
+      require => File["${puppetmaster::confdir}/puppet.conf"],
+    }
   }
 }
